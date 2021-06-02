589112b515867eb5ca6a8aba19d22d6185ee6aad
SeppeV <seppe.vuerstaek@outlook.com>
2021-06-02 09:29:32 +0200

    Scoreboard

--- main.py
+++ main.py
@@ -18,55 +18,92 @@ HEIGHT = 600
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-maaseik = pygame.image.load("Maaseik.jpg")
-Roeselare= pygame.image.load("Knack.jpg")
-Roeselare = pygame.transform.smoothscale(Roeselare,(250, 175))
+achtergrond = pygame.image.load("achtergrond.png")
+achtergrond = pygame.transform.smoothscale(achtergrond,(800, 600))
+maaseik = pygame.image.load("maaseiktr.png")
+maaseik = pygame.transform.smoothscale(maaseik,(200, 150))
+Roeselare= pygame.image.load("roeselaretr.png")
+Roeselare = pygame.transform.smoothscale(Roeselare,(200, 150))
 
 # TODO: Laad hier je font
 # font = ...
 
 score1 = 0
 score2 = 0
+thuisset = 0
+uitset = 0
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
-
-            if event.key == pygame.K_LEFT:
+            if event.key == pygame.K_p:
+                running = False
+            elif event.key == pygame.K_LEFT:
                 score1 += 1
+                if score1 >= 25:
+                    if score2 < score1 - 1:
+                        score1 = 0
+                        score2 = 0
+                        thuisset += 1
+                if uitset == 2 and thuisset == 2:
+                    if score2 == 15:
+                        if score2 < score1 - 1:
+                            thuisset += 1
             elif event.key == pygame.K_RIGHT:
                 score2 += 1
+                if score2 >= 25:
+                    if score1 < score2 - 1:
+                        score1 = 0
+                        score2 = 0
+                        uitset += 1
+                if uitset == 2 and thuisset == 2:
+                    if score2 == 15:
+                        if score1 < score2 - 1:
+                            uitset += 1
             elif event.key == pygame.K_r:
                 score1 = 0
+                thuisset = 0
                 score2 = 0
+                uitset = 0
+
+
 
 
     screen.fill("white")
-    screen.blit(maaseik, (100, 80))
-    screen.blit(Roeselare, (515, 110))
+    screen.blit(achtergrond, (0, 0))
+    screen.blit(maaseik, (130, 70))
+    screen.blit(Roeselare, (470, 70))
 
-    font = pygame.font.SysFont("comicsansms", 24)
+
+    font = pygame.font.SysFont("comicsansms", 45)
     text = font.render(f"{score1}", True, "black")
     h = text.get_height()
     w = text.get_width()
-    screen.blit(text,(WIDTH/4-w/2 ,HEIGHT/1.25-h/2))
+    screen.blit(text,(WIDTH/4-w/2 ,HEIGHT/1.5-h/2))
 
 
 
-    font = pygame.font.SysFont("comicsansms", 24)
+    font = pygame.font.SysFont("comicsansms", 45)
     text = font.render(f"{score2}", True, "black")
     h = text.get_height()
     w = text.get_width()
-    screen.blit(text, (WIDTH / 1.25 - w / 3, HEIGHT / 1.25 - h / 2))
+    screen.blit(text, (WIDTH / 1.35 - w / 3, HEIGHT / 1.5 - h / 2))
 
-    font = pygame.font.SysFont("comicsansms", 35)
-    text = font.render(f"    {ploegnaamA}              {ploegnaamB}", True, "black")
+    font = pygame.font.SysFont("comicsansms", 24)
+    text = font.render(f"{thuisset}", True, "black")
     h = text.get_height()
     w = text.get_width()
-    screen.blit(text, (WIDTH / 3 - w / 3, HEIGHT /  1.75 - h / 2))
+    screen.blit(text, (WIDTH / 4 - w / 3, HEIGHT / 1.25 - h / 2))
 
+
+    font = pygame.font.SysFont("comicsansms", 24)
+    text = font.render(f"{uitset}", True, "black")
+    h = text.get_height()
+    w = text.get_width()
+    screen.blit(text, (WIDTH / 1.35 - w / 3, HEIGHT / 1.25 - h / 2))
 
     font = pygame.font.SysFont("comicsansms", 25)
     text = font.render(f"{titel}", True, "black")
