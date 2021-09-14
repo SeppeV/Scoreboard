3605f510c3f8e3d25255f831695340254a81cfb5
SeppeV <seppe.vuerstaek@outlook.com>
2021-09-14 09:31:45 +0200

    Scoreboard

---
+++ Game.py
@@ -0,0 +1,44 @@
+class Game:
+
+    def __init__(self):
+        self.points_home = 0
+        self.points_away = 0
+        self.sets_home = 0
+        self.sets_away = 0
+        self.set_nr = 1
+        self.reset = 0
+
+    def get_max_points(self):
+        if self.set_nr < 5:
+            return 25
+        else:
+            return 15
+
+    def score(self, team):
+
+        if team == "H":
+            self.points_home += 1
+
+        if team == "A":
+            self.points_away += 1
+
+        if team == "R":
+            self.points_home = 0
+            self.points_away = 0
+            self.sets_home = 0
+            self.sets_away = 0
+
+        if self.points_home >= self.get_max_points():
+            if self.points_away < self.points_home - 1:
+                self.points_home = 0
+                self.points_away = 0
+                self.sets_home += 1
+                self.set_nr += 1
+
+        if self.points_away >= self.get_max_points():
+            if self.points_home < self.points_away - 1:
+                self.points_home = 0
+                self.points_away = 0
+                self.sets_away += 1
+                self.set_nr += 1
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-
 import configparser
+from Game import Game
+
 config = configparser.ConfigParser()
 config.read("config.ini")
 ploegnaamA = config["teamA"]["naam"]
@@ -28,6 +29,7 @@ Roeselare = pygame.transform.smoothscale(Roeselare,(200, 150))
 # TODO: Laad hier je font
 #title_font = pygame.font.font("texttype.ttf", 45)
 
+score = Game()
 score1 = 0
 score2 = 0
 thuisset = 0
@@ -42,32 +44,11 @@ while running:
             if event.key == pygame.K_p:
                 running = False
             elif event.key == pygame.K_LEFT:
-                score1 += 1
-                if score1 >= 25:
-                    if score2 < score1 - 1:
-                        score1 = 0
-                        score2 = 0
-                        thuisset += 1
-                if uitset == 2 and thuisset == 2:
-                    if score2 == 15:
-                        if score2 < score1 - 1:
-                            thuisset += 1
+                score.score("A")
             elif event.key == pygame.K_RIGHT:
-                score2 += 1
-                if score2 >= 25:
-                    if score1 < score2 - 1:
-                        score1 = 0
-                        score2 = 0
-                        uitset += 1
-                if uitset == 2 and thuisset == 2:
-                    if score2 == 15:
-                        if score1 < score2 - 1:
-                            uitset += 1
+                score.score("H")
             elif event.key == pygame.K_r:
-                score1 = 0
-                thuisset = 0
-                score2 = 0
-                uitset = 0
+                score.score("R")
 
 
     screen.fill("white")
@@ -77,25 +58,25 @@ while running:
 
 
     font = pygame.font.SysFont("comicsansms", 45)
-    text = font.render(f"{score1}", True, "black")
+    text = font.render(f"{score.points_away}", True, "black")
     h = text.get_height()
     w = text.get_width()
     screen.blit(text,(WIDTH/4-w/2 ,HEIGHT/1.5-h/2))
 
     font = pygame.font.SysFont("comicsansms", 45)
-    text = font.render(f"{score2}", True, "black")
+    text = font.render(f"{score.points_home}", True, "black")
     h = text.get_height()
     w = text.get_width()
     screen.blit(text, (WIDTH / 1.35 - w / 3, HEIGHT / 1.5 - h / 2))
 
     font = pygame.font.SysFont("comicsansms", 24)
-    text = font.render(f"{thuisset}", True, "black")
+    text = font.render(f"{score.sets_away}", True, "black")
     h = text.get_height()
     w = text.get_width()
     screen.blit(text, (WIDTH / 4 - w / 3, HEIGHT / 1.25 - h / 2))
 
     font = pygame.font.SysFont("comicsansms", 24)
-    text = font.render(f"{uitset}", True, "black")
+    text = font.render(f"{score.sets_home}", True, "black")
     h = text.get_height()
     w = text.get_width()
     screen.blit(text, (WIDTH / 1.35 - w / 3, HEIGHT / 1.25 - h / 2))
